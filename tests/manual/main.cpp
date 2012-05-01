b36f04565f3fb618c5291ceabc61de5fb5930ae0
Sebastian Sauer <mail@dipe.org>
2012-05-01 17:31:22 +0700

    Use better appname

--- tests/manual/main.cpp
+++ tests/manual/main.cpp
@@ -25,6 +25,7 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    app.setApplicationName(QLatin1String("TestApp"));
 
     MainWindow *window = new MainWindow;
     window->show();
