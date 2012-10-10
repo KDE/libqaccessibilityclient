f329a764fedc901fa39fcbf887d25bf626b499d0
Sebastian Sauer <sebastian.sauer@kdab.com>
2012-10-10 16:22:06 +0700

    Initial implementation of role objects to provide a more high level API
    This will be the base of the widgets docker to rebuild the
    applications UI using Qt widgets. Still lot to do but its
    a start. Note that the new API is not public yet till more
    ready.

---
+++ src/qaccessibilityclient/roleobjects.cpp
@@ -0,0 +1,234 @@
+/*
+    Copyright 2012 Sebastian Sauer <sebastian.sauer@kdab.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "roleobjects.h"
+
+using namespace QAccessibleClient;
+
+namespace QAccessibleClient {
+
+class ObjectRole::Private
+{
+public:
+    AccessibleObject m_acc;
+    AccessibleObject::Role m_role;
+    Private(const AccessibleObject &acc, AccessibleObject::Role role) : m_acc(acc), m_role(role) {}
+};
+
+}
+
+ObjectRole::ObjectRole(const AccessibleObject &acc, AccessibleObject::Role role)
+    : QObject()
+    , d(new Private(acc, role))
+{
+}
+
+ObjectRole::ObjectRole(Private *dd)
+    : QObject()
+    , d(dd)
+{
+}
+
+ObjectRole::~ObjectRole()
+{
+    delete d;
+}
+
+AccessibleObject ObjectRole::accessible() const
+{
+    return d->m_acc;
+}
+
+AccessibleObject::Role ObjectRole::role() const
+{
+    return d->m_role;
+}
+
+QString ObjectRole::roleName() const
+{
+    return d->m_acc.roleName();
+}
+
+QString ObjectRole::localizedRoleName() const
+{
+    return d->m_acc.localizedRoleName();
+}
+
+QString ObjectRole::url() const
+{
+    return d->m_acc.url().toString();
+}
+
+QString ObjectRole::name() const
+{
+    return d->m_acc.name();
+}
+
+QString ObjectRole::description() const
+{
+    return d->m_acc.description();
+}
+
+QRect ObjectRole::boundingRect() const
+{
+    return d->m_acc.boundingRect();
+}
+
+ObjectRole* ObjectRole::create(const AccessibleObject &acc)
+{
+    return create(acc, acc.role());
+}
+
+ObjectRole* ObjectRole::create(const AccessibleObject &acc, AccessibleObject::Role role)
+{
+    switch (role) {
+        case AccessibleObject::CheckBox: return new CheckBoxRole(acc);
+        // case AccessibleObject::CheckableMenuItem:
+        // case AccessibleObject::ColumnHeader:
+        case AccessibleObject::ComboBox: return new ComboBoxRole(acc);
+        // case AccessibleObject::DesktopFrame:
+        // case AccessibleObject::Dial:
+        // case AccessibleObject::Dialog:
+        // case AccessibleObject::Filler:
+        // case AccessibleObject::Frame:
+        // case AccessibleObject::Icon:
+        case AccessibleObject::Label: return new LabelRole(acc);
+        // case AccessibleObject::ListView:
+        // case AccessibleObject::ListItem:
+        // case AccessibleObject::Menu:
+        // case AccessibleObject::MenuBar:
+        // case AccessibleObject::MenuItem:
+        // case AccessibleObject::Tab:
+        // case AccessibleObject::TabContainer:
+        // case AccessibleObject::PasswordText:
+        // case AccessibleObject::PopupMenu:
+        // case AccessibleObject::ProgressBar:
+        case AccessibleObject::Button: return new ButtonRole(acc);
+        case AccessibleObject::RadioButton: return new RadioButtonRole(acc);
+        // case AccessibleObject::RadioMenuItem:
+        // case AccessibleObject::RowHeader:
+        // case AccessibleObject::ScrollBar:
+        // case AccessibleObject::ScrollArea:
+        // case AccessibleObject::Separator:
+        // case AccessibleObject::Slider:
+        // case AccessibleObject::SpinButton:
+        // case AccessibleObject::StatusBar:
+        // case AccessibleObject::TableView:
+        // case AccessibleObject::TableCell:
+        // case AccessibleObject::TableColumnHeader:
+        // case AccessibleObject::TableColumn:
+        // case AccessibleObject::TableRowHeader:
+        // case AccessibleObject::TableRow:
+        // case AccessibleObject::Terminal:
+        case AccessibleObject::Text: return new TextRole(acc);
+        case AccessibleObject::ToggleButton: return new ToggleRole(acc);
+        // case AccessibleObject::ToolBar:
+        // case AccessibleObject::ToolTip:
+        // case AccessibleObject::TreeView:
+        // case AccessibleObject::Window:
+        // case AccessibleObject::TreeItem:
+    }
+    return 0;
+}
+
+CheckBoxRole::CheckBoxRole(const AccessibleObject &acc)
+    : ObjectRole(acc, AccessibleObject::CheckBox)
+{
+}
+
+QString CheckBoxRole::text() const
+{
+    return accessible().name();
+}
+
+ComboBoxRole::ComboBoxRole(const AccessibleObject &acc)
+    : ObjectRole(acc, AccessibleObject::ComboBox)
+{
+}
+
+QString ComboBoxRole::text() const
+{
+    return accessible().name();
+}
+
+LabelRole::LabelRole(const AccessibleObject &acc)
+    : ObjectRole(acc, AccessibleObject::Label)
+{
+}
+
+QString LabelRole::text() const
+{
+    return accessible().name();
+}
+
+ButtonRole::ButtonRole(const AccessibleObject &acc)
+    : ObjectRole(acc, AccessibleObject::Button)
+{
+}
+
+QString ButtonRole::text() const
+{
+    return accessible().name();
+}
+
+RadioButtonRole::RadioButtonRole(const AccessibleObject &acc)
+    : ObjectRole(acc, AccessibleObject::RadioButton)
+{
+}
+
+QString RadioButtonRole::text() const
+{
+    return accessible().name();
+}
+
+TextRole::TextRole(const AccessibleObject &acc)
+    : ObjectRole(acc, AccessibleObject::Textt)
+{
+}
+
+QString TextRole::text() const
+{
+    if (accessible().supportedInterfaces().testFlag(AccessibleObject::TextInterface))
+        return accessible().text();
+    return accessible().name();
+}
+
+void TextRole::setText(const QString &text)
+{
+    if (isEditable())
+        accessible().setText(text);
+    else
+        qWarning() << "Cannot TextRole::setText on none editable text object";
+}
+
+bool TextRole::isEditable() const
+{
+    return accessible().supportedInterfaces().testFlag(AccessibleObject::EditableTextInterface);
+}
+
+ToggleButtonRole::ToggleButtonRole(const AccessibleObject &acc)
+    : ObjectRole(acc, AccessibleObject::ToggleButton)
+{
+}
+
+QString ToggleButtonRole::text() const
+{
+    return accessible().name();
+}
---
+++ src/qaccessibilityclient/roleobjects.h
@@ -0,0 +1,211 @@
+/*
+    Copyright 2012 Sebastian Sauer <sebastian.sauer@kdab.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef QACCESSIBILITYCLIENT_ROLEOBJECTS_H
+#define QACCESSIBILITYCLIENT_ROLEOBJECTS_H
+
+#include <QObject>
+#include <QRect>
+
+#include "qaccessibilityclient_export.h"
+#include "accessibleobject.h"
+
+namespace QAccessibleClient {
+
+/**
+    Base class for role objects.
+
+    The role objects offer a high level API to deal with accessible
+    objects. Every accessible objects has a role assigned and that
+    role is used with the role objects to create one specialized
+    role object instance per accessible object. The API given is
+    Qt-ified using the QMetaObject system to allow introspection.
+*/
+class QACCESSIBILITYCLIENT_EXPORT ObjectRole : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(AccessibleObject accessible READ accessible)
+    Q_PROPERTY(AccessibleObject::Role role READ role)
+    Q_PROPERTY(QString roleName READ roleName)
+    Q_PROPERTY(QString localizedRoleName READ localizedRoleName)
+    Q_PROPERTY(QString url READ url)
+    Q_PROPERTY(QString name READ name)
+    Q_PROPERTY(QString description READ description)
+    Q_PROPERTY(QRect boundingRect READ boundingRect)
+public:
+    virtual ~ObjectRole();
+
+    AccessibleObject accessible() const;
+    AccessibleObject::Role role() const;
+    QString roleName() const;
+    QString localizedRoleName() const;
+    QString url() const;
+    QString name() const;
+    QString description() const;
+    QRect boundingRect() const;
+
+    static ObjectRole* create(const AccessibleObject &acc);
+    static ObjectRole* create(const AccessibleObject &acc, AccessibleObject::Role role);
+
+protected:
+    class Private;
+    Private *d;
+
+    ObjectRole(const AccessibleObject &acc, AccessibleObject::Role role);
+    ObjectRole(Private *dd);
+};
+
+/**
+    CheckBox role object.
+*/
+class CheckBoxRole : public ObjectRole
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text)
+public:
+    explicit CheckBoxRole(const AccessibleObject &acc);
+    QString text() const;
+};
+
+// CheckableMenuItem
+// ColumnHeader
+
+/**
+    ComboBox role object.
+*/
+class ComboBoxRole : public ObjectRole
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text)
+public:
+    explicit ComboBoxRole(const AccessibleObject &acc);
+    QString text() const;
+};
+
+// DesktopFrame
+// Dial
+// Dialog
+// Filler
+// Frame
+// Icon
+
+/**
+    Label role object.
+*/
+class LabelRole : public ObjectRole
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text)
+public:
+    explicit LabelRole(const AccessibleObject &acc);
+    QString text() const;
+};
+
+// ListView
+// ListItem
+// Menu
+// MenuBar
+// MenuItem
+// Tab
+// TabContainer
+// PasswordText
+// PopupMenu
+// ProgressBar
+
+/**
+    Button role object.
+*/
+class ButtonRole : public ObjectRole
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text)
+public:
+    explicit ButtonRole(const AccessibleObject &acc);
+    QString text() const;
+};
+
+/**
+    RadioButton role object.
+*/
+class RadioButtonRole : public ObjectRole
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text)
+public:
+    explicit RadioButtonRole(const AccessibleObject &acc);
+    QString text() const;
+};
+
+// RadioMenuItem
+// RowHeader
+// ScrollBar
+// ScrollArea
+// Separator
+// Slider
+// SpinButton
+// StatusBar
+// TableView
+// TableCell
+// TableColumnHeader
+// TableColumn
+// TableRowHeader
+// TableRow
+// Terminal
+
+/**
+    Text role object.
+*/
+class TextRole : public ObjectRole
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text WRITE setText)
+    Q_PROPERTY(bool isEditable READ isEditable)
+public:
+    explicit TextRole(const AccessibleObject &acc);
+    QString text() const;
+    bool isEditable() const;
+public slots:
+    void setText(const QString &text);
+};
+
+/**
+    ToggleButton role object.
+*/
+class ToggleButtonRole : public ObjectRole
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text)
+public:
+    explicit ToggleButtonRole(const AccessibleObject &acc);
+    QString text() const;
+};
+
+// ToolBar
+// ToolTip
+// TreeView
+// Window
+// TreeItem
+
+}
+
+//Q_DECLARE_METATYPE(QAccessibleClient::ObjectRole)
+
+#endif
+
