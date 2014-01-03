8039c44b6c58526033d3c5a03013a92d0d3ab8d4
Frederik Gladhorn <frederik.gladhorn@digia.com>
2014-01-03 20:45:59 +0100

    Make roleobjects private

--- src/qaccessibilityclient/roleobjects.cpp
+++ src/qaccessibilityclient/roleobjects.cpp
@@ -18,7 +18,7 @@
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "roleobjects.h"
+#include "roleobjects_p.h"
 
 using namespace QAccessibleClient;
 
